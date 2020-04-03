99113171847c0b96496def1362685a09a7f45847
Arne Schirmacher <arne@schirmacher.de>
2020-04-03 07:50:55 +0200

    switched from constructor based to @Autowired based variable initialization

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -2,6 +2,7 @@ package guru.springframework.controllers;
 
 import guru.springframework.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,11 +14,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
-    private final RecipeService recipeService;
-
-    public IndexController(RecipeService recipeService) {
-        this.recipeService = recipeService;
-    }
+    @Autowired
+    private RecipeService recipeService;
 
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(Model model) {
